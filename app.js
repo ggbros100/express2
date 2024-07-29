d9e36b9b7ab71a6638576aa3ff7df5340708cb87
ggbros100 <kimura-t@atjc-it.jp>
2024-07-29 14:33:37 +0900

    a

--- app.js
+++ app.js
@@ -1,94 +1,47 @@
+var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var session = require('express-session');
-var MongoClient = require('mongodb').MongoClient;
+var logger = require('morgan');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var helloRouter = require('./routes/hello');
 var notesRouter = require('./routes/notes');
 var catRouter = require('./routes/cat');
 
-var Users = require('./models/user');
-var Items = require('./models/item');
-var Store = require('./models/store');
-var StoreItem = require('./models/storeitem');
-
 var app = express();
-//set mongo db connection
-var db = mongoose.connection; 
-
-MongoClient.connect("mongodb://localhost:27017/test", function(err, db) {
-  if(!err) {
-    console.log("We are connected");
-  }
-});
-// var MONGOHQ_URL="mongodb://localhost:27017/test" 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(session({
-    secret: 'something',
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
 app.use('/notes', notesRouter);
 app.use('/cat', catRouter);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//     var err = new Error('Not Found');
-//     err.status = 404;
-//     next(err);
-// });
-
-// Make our db accessible to our router
-app.use(function(req, res, next){
-  req.db = db;
-  next();
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
+// error handler
 app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
-
-module.exports = router;
+module.exports = app;
