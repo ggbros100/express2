7b8d1c63d0c7ac46c5e378b4936fa258f0520bdb
ggbros100 <kimura-t@atjc-it.jp>
2024-07-29 14:08:15 +0900

    change page

--- routes/cat.js
+++ routes/cat.js
@@ -1,9 +1,9 @@
-constrequest= require('request');
- request('https://api.thecatapi.com/v1/images/search'), function(error, response, body) {
- if(!error&& response.statusCode== 200) {
- constdata= JSON.parse(body);
- console.log(body);
- constcatImageUrl= data[0].url;
- console.log(catImageUrl);
- }
- };
+const request = require('request');
+request('https://api.thecatapi.com/v1/images/search', function(error, response, body) {
+    if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body);
+        console.log(body);
+        const catImageUrl = data[0].url;
+        console.log(catImageUrl);
+    }
+});
